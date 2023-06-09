500e6d5cb04932660c88ed4c318b57f180745363
niwaguan <niwaguan@gmail.com>
2023-06-09 15:50:21 +0800

    cs2go style

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/stormyang/fvm/versions/3.7.11"
+export "FLUTTER_APPLICATION_PATH=/Users/stormyang/OpenSource/CountryCodePicker"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=2.0.2"
+export "FLUTTER_BUILD_NUMBER=2.0.2"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
